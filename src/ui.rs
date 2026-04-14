47c7f6e126fca172244a0c4ade27e7bf7eab30a3
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2026-04-14 00:44:25 -0500

    implementamos la tui

--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,16 @@
-use std::io::{Error, Stdout, Write};
-use termion::clear;
+use std::io::{Error, Stdout, Write, stdin, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::{clear, raw::IntoRawMode};
 
-struct UI {
+pub struct UI {
     options: Vec<String>,
     selected: usize,
     title: String,
 }
 
 impl UI {
-    fn new(title: &str, options: Vec<String>) -> Self {
+    pub fn new(title: &str, options: Vec<String>) -> Self {
         UI {
             title: title.to_string(),
             options,
@@ -44,5 +46,31 @@ impl UI {
 
         stdout.flush()?;
         Ok(())
+    }
+
+    pub fn show(&mut self) -> Result<Option<usize>, Error> {
+        let mut stdout = stdout().into_raw_mode()?;
+
+        self.draw(&mut stdout)?;
+
+        for key in stdin().keys() {
+            match key? {
+                Key::Char('q') => return Ok(None),
+                Key::Up => {
+                    if self.selected > 0 {
+                        self.selected -= 1;
+                    }
+                }
+                Key::Down => {
+                    if self.selected < self.options.len() - 1 {
+                        self.selected += 1;
+                    }
+                }
+                Key::Char('\n') => return Ok(Some(self.selected)),
+                _ => {}
+            }
+            self.draw(&mut stdout)?;
+        }
+        Ok(None)
     }
 }
